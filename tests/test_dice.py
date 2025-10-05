02ea8ae80b753cb359c9aa944f5e9b50331e3efd
ellenvii <ellenviira03@gmail.com>
2025-10-05 10:34:52 +0200

    Basic Dice.py and Test_Dice()
    Added simple Die class and method for testing roll range. Passed 100% in venv.

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -1,2 +1,16 @@
-pass
+"""
+Dice class testing
+"""
 
+from dice import Die
+
+class TestDie:
+    """
+    Tests if the dice roll is in range
+    """
+    def test_roll_range(self):
+        d = Die()
+        for i in range(20):
+            x = d.roll()
+            assert 1 <= x <= 6
+    
