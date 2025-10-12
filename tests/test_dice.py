c987820722c6bc8cf1032d070f840b0b56539e45
amanda <amandafuentes831@gmail.com>
2025-10-13 00:54:18 +0200

    some test cases for dice class

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -14,3 +14,35 @@ class TestDie:
             x = d.roll()
             assert 1 <= x <= 6
     
+    def test_resturns_integer():
+        """
+        
+        """
+        dice = Die()
+        result = dice.roll()
+        assert isinstance(result, int)
+
+    def test_default_sides_to_six():
+        """
+        
+        """
+        dice = Die()
+        assert dice.sides == 6
+
+    def test_rolls_are_different():
+        """
+        
+        """
+        dice = Die(6)
+        results = set()
+        for _ in range(50):
+            results.add(dice.roll())
+        assert len(results) > 1
+
+    #  to-do: add test for remembering last roll
+
+
+
+
+
+    
